4f10757b3b8784af395a08052a40bb08c7feb10d
Arpan NLN <arpannln@ucla.edu>
2018-06-07 18:19:15 -0700

    just gotta integrate API calls now

--- exptrial/screens/GIFScreen.js
+++ exptrial/screens/GIFScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Button } from 'react-native-elements';
-import { ScrollView, StyleSheet, ImageBackground, Image } from 'react-native';
+import { ScrollView, StyleSheet, ImageBackground, Image, TextInput } from 'react-native';
 
 export default class GIFScreen extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+  
   static navigationOptions = {
     title: 'GIF',
   };
@@ -11,7 +17,30 @@ export default class GIFScreen extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <ImageBackground style={styles.container}>
-        
+        <Image
+            style={styles.imageStyles}
+            source={{uri: 'https://media1.giphy.com/media/xT4uQ6HaStH5UX7EBO/giphy.gif'}}
+          />
+        <TextInput
+          style={styles.inputStyles}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+          defaultValue='Search'
+        />
+        <Button
+          onPress={() => navigate('GIF')}
+          buttonStyle={[ styles.buttonStyle, { backgroundColor: "#0099cc" } ]}
+          title="SEARCH"
+          color="white"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={() => navigate('GIF')}
+          buttonStyle={[ styles.buttonStyle, { backgroundColor: "#FF1493" } ]}
+          title="RANDOM"
+          color="white"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </ImageBackground>
     );
   }
@@ -33,5 +62,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
   },
+  imageStyles: {
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
+  },
+  inputStyles: {
+    backgroundColor: 'white',
+    height: 40, 
+    width: 200,
+    marginLeft: 90,
+    marginTop: 15,
+  }
 });
 
